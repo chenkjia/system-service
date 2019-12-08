3a44b4be4132223babeb6ad9de1ce76664acd739
chenkj <>
2019-12-08 16:58:31 +0800

    查询函数分解

--- models/dictionary.js
+++ models/dictionary.js
@@ -3,19 +3,21 @@ const db = require('../lib/mongo.js');
 const CommonSchema = require('./common.js');
 
 //一个字典选项模型
-const OptionSchema = new mongoose.Schema({
-  value: { type: String, unique: true }, // 选项值
-  label: String // 选项文字
-})
+// const OptionSchema = new mongoose.Schema({
+//   value: { type: String}, // 选项值
+//   label: String // 选项文字
+// })
 //一个字典模型
 const Schema = new mongoose.Schema({
   ...CommonSchema,
   sign: { type: String, unique: true },  // 字典资源标识
   label: { type: String },  // 字典资源名称
-  options: [OptionSchema],
+  options: [{
+    value: { type: String}, // 选项值
+    label: { type: String} // 选项文字
+  }],
   enabled: { type: Boolean }, // 启用状态
   remark: { type: String },  // 备注
 });
-
 const Modal = db.model("dictionary", Schema);
 module.exports = Modal
